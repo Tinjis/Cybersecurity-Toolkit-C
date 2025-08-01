be688b8519a31d7cc5588b98d3127684ffe32164
cybertrojan <thecybertrojan@gmail.com>
2025-08-01 21:08:56 +0100

    Update server.c

--- 001-network-tools/001-tcp-client-server/server.c
+++ 001-network-tools/001-tcp-client-server/server.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
